9f6304b2704d32817f9d061ed9fd015f7897b8f0
DanielGoldenberg <goldenbergdaniel8@gmail.com>
2021-08-21 15:40:45 -0400

    Added vector arithmetic functions

--- src/util/umath.c
+++ src/util/umath.c
@@ -2,8 +2,44 @@
 
 #include "umath.h"
 
-f32 min(f32 x, f32 y) 
+f32 min(f32 num1, f32 num2) 
 {
-    if (x < y) return x;
-    return y;
+    if (num1 < num2) return num1;
+    return num2;
+}
+
+v2 add_v2(v2 vec1, v2 vec2)
+{
+    v2 vec3;
+    vec3.x = vec1.x + vec2.x;
+    vec3.y = vec1.y + vec2.y;
+
+    return vec3;
+}
+
+v2 sub_v2(v2 vec1, v2 vec2)
+{
+    v2 vec3;
+    vec3.x = vec1.x - vec2.x;
+    vec3.y = vec1.y - vec2.y;
+
+    return vec3;
+}
+
+v2 mult_v2(v2 vec1, v2 vec2)
+{
+    v2 vec3;
+    vec3.x = vec1.x * vec2.x;
+    vec3.y = vec1.y * vec2.y;
+
+    return vec3;
+}
+
+v2 div_v2(v2 vec1, v2 vec2)
+{
+    v2 vec3;
+    vec3.x = vec1.x / vec2.x;
+    vec3.y = vec1.y / vec2.y;
+
+    return vec3;
 }
--- src/util/umath.h
+++ src/util/umath.h
@@ -1,4 +1,16 @@
 #pragma once
 
-// Returns min between x and y
-f32 min(f32 x, f32 y);
+// Returns min between num1 and num2
+f32 min(f32 num1, f32 num2); 
+
+// Adds vectors vec1 and vec2
+v2 add_v2(v2 vec1, v2 vec2);
+
+// Subtracts vectors vec1 and vec2
+v2 sub_v2(v2 vec1, v2 vec2);
+
+// Multiplies vectors vec1 and vec2
+v2 mult_v2(v2 vec1, v2 vec2);
+
+// Divides vectors vec1 and vec2
+v2 div_v2(v2 vec1, v2 vec2);
