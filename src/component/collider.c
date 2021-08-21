21f7884e81912d660e7fcaa56a5467441173ee82
DanielGoldenberg <goldenbergdaniel8@gmail.com>
2021-08-21 15:39:35 -0400

    Added BoxCollider component

---
+++ src/component/collider.c
@@ -0,0 +1,22 @@
+#include "../globals.h"
+
+#include "../util/umath.h"
+#include "collider.h"
+
+BoxCollider box_collider_new(v2 offset, f32 width, f32 height, bool is_trigger)
+{
+    BoxCollider col = {
+        (v2) {0, 0},
+        offset,
+        width,
+        height,
+        is_trigger
+    };
+
+    return col;
+}
+
+void box_collider_update(BoxCollider* this, v2 parent_pos)
+{
+    this->position = add_v2(parent_pos, this->offset);
+}
