f68fddf365ddff6a7204ad6af12969e51fdb2edd
DanielGoldenberg <goldenbergdaniel8@gmail.com>
2021-08-31 21:28:08 -0400

    Added dot product function (probably wrong)

--- src/util/umath.c
+++ src/util/umath.c
@@ -1,6 +1,7 @@
 #include "../globals.h"
 
 #include "umath.h"
+#include "math.h"
 
 f32 min(f32 num1, f32 num2) 
 {
@@ -12,6 +13,11 @@ f32 max(f32 num1, f32 num2)
 {
     if (num1 > num2) return num1;
     return num2;
+}
+
+f32 distance(f32 num1, f32 num2)
+{
+    return sqrt(pow(num1, 2) + pow(num2, 2));
 }
 
 v2 add_v2(v2 vec1, v2 vec2)
@@ -30,4 +36,14 @@ v2 sub_v2(v2 vec1, v2 vec2)
     vec3.y = vec1.y - vec2.y;
 
     return vec3;
+}
+
+f32 mult_v2(v2 vec1, v2 vec2)
+{
+    f32 numerator = (vec1.x * vec2.x) + (vec1.y * vec2.y);
+    f32 denominator = distance(vec1.x, vec1.y) * distance(vec2.x, vec2.y);
+
+    f32 angle = acos(numerator / denominator);
+
+    return ((vec1.x * vec2.x) + (vec1.y * vec2.y)) * cos(angle);
 }
--- src/util/umath.h
+++ src/util/umath.h
@@ -6,11 +6,14 @@ f32 min(f32 num1, f32 num2);
 // Returns max between two numbers
 f32 max(f32 num1, f32 num2); 
 
+// Returns distance between two numbers
+f32 distance(f32 num1, f32 num2); 
+
 // Adds two vectors
 v2 add_v2(v2 vec1, v2 vec2);
 
 // Subtracts two vectors
 v2 sub_v2(v2 vec1, v2 vec2);
 
-// Multiplies two vectors
-v2 mult_v2(v2 vec1, v2 vec2);
+// Multiplies two vectors (dot product)
+f32 mult_v2(v2 vec1, v2 vec2);
